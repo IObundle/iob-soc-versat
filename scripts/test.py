a190eaa65771323e6e9f13ed7f7ea0f9be6008dc
z <ruben.teixeira@tecnico.ulisboa.pt>
2025-01-15 21:35:19 +0000

    Added timeouts but without proper reporting yet, change to threads, some future planning in TODOs

--- scripts/test.py
+++ scripts/test.py
@@ -1,4 +1,5 @@
 import multiprocessing
+from multiprocessing.pool import ThreadPool as ThreadPool
 import subprocess as sp
 import sys
 import json
@@ -6,6 +7,8 @@ import os
 import codecs
 from dataclasses import dataclass,fields
 from enum import Enum
+
+SIMULATE = False
 
 ##############
 # TODO: Add a brief description here of what we are doing
@@ -18,7 +21,26 @@ from enum import Enum
 # Add the .cpp file to the tokenizer and hasher otherwise we run the risk of having false results for tests that only have software problems.
 # Add some commands to the tool that allow us to force stuff, like forcing the tokens/hash of a bad test case and stuff like that.
 # Need to provide some form of progress reporting on the terminal
-
+#   Could be something as simple as a print that uses \r to update and that displays how many tests are running, how many compiled correctly, how many passed pc-emul and so on.
+# Maybe useful to group tests that are similar into groups and take groups into account when outputting stuff.
+# Need to change the way we are parallelizing the tests. I first want to run all the hash/tokenizer checks before starting a single pc-emul run.
+#   At the same time, I do not want to prevent threads from starting to run pc-emul because they are waiting for a hash computation to finish (same thing with sim-run waiting for pc-emul)
+#     Before progressing further, need to change Pool to a work based architecture.
+#     Besides, using the Pool appears to not solve the problem of left over python executables 
+#     
+# Can easily add per test information. Something like custom timeouts for commands and stuff like that.
+#
+# For things like architecture change at the hardware level, it should be something that is done for all the tests at the same time.
+#   Our maybe it is better if we do it by group. Things like AXI DATA_W changing does not affect config share and stuff like that, so no point in running those tests for those cases. We want to run the VRead/VWrite tests and stuff like that
+#
+# Need to catch the exceptions by timeout and report to the user that a timeout occured (and at what stage).
+#
+# Check if we can run sim-run without 
+#
+# Can start saving the average amount of time that a test took to run and then use that information to change the order to run the fastest tests first (with more )
+#
+# Add proper parsing so we can embed comments into the tests
+#
 # Order of these are important. Assuming that failing sim-run means that passed pc-emul 
 # TODO: Maybe make enum, depending on what python enums offer us
 class Stage(Enum):
@@ -113,7 +135,7 @@ def RunVersat(testName,testFolder,versatExtra):
 
    result = None
    try:
-      result = sp.run(args,capture_output=True)
+      result = sp.run(args,capture_output=True,timeout=10) # Maybe a bit low for merge based tests, eventually add timeout 'option' to the test itself
    except Exception as e:
       print(f"Except on calling Versat:{e}") # This should not happen
       return VersatResult(True,[])
@@ -139,7 +161,7 @@ def ComputeFilesTokenSizeAndHash(files):
 
    result = None
    try:
-      result = sp.run(args,capture_output=True)
+      result = sp.run(args,capture_output=True,timeout=5)
    except Exception as e:
       print(f"Except on ComputeHash:{e}")
       return -1,-1
@@ -162,7 +184,10 @@ def RunMakefile(target,testName):
       args = ["make",target,f"TEST={testName}"]
 
       command = f"make clean pc-emul-run TEST={testName}"
-      result = sp.run(command,capture_output=True,shell=True)
+      result = sp.run(command,capture_output=True,shell=True,timeout=60) # 60
+   # TODO: Add proper error propragation information so we know we time outted.
+   #except subprocess.TimeoutExpired as t:
+   #   return -1,""
    except Exception as e:
       print(f"Except on calling makefile:{e}")
       return -1,""
@@ -178,12 +203,18 @@ def PerformTest(test,stage):
    if stage == Stage.PC_EMUL:
       returnCode,output = RunMakefile("pc-emul-run",test)
 
+      if(returnCode != 0):
+         return returnCode,0,0
+
       filepathList = FindAndParseFilepathList(output)
       tokens,hashVal = ComputeFilesTokenSizeAndHash(filepathList)
 
       return returnCode,tokens,hashVal
    if stage == Stage.SIM_RUN:
       returnCode,output = RunMakefile("sim-run",test)
+
+      if(returnCode != 0):
+         return returnCode,0,0
 
       filepathList = FindAndParseFilepathList(output)
       tokens,hashVal = ComputeFilesTokenSizeAndHash(filepathList)
@@ -217,11 +248,24 @@ def ProcessOneTest(testWithIndex):
 
    testTempDir = TempDir(name)
 
-   stage = Stage[test.get('stage',"NOT_WORKING")]
-   finalStage = Stage[test.get('finalStage',"NOT_WORKING")]
+   stage = Stage.NOT_WORKING
+   finalStage = Stage.NOT_WORKING
+   try:
+      stage = Stage[test.get('stage',"NOT_WORKING")]
+      finalStage = Stage[test.get('finalStage',"NOT_WORKING")]
+   except Exception as e:
+      print("Error on Process One Test, probably a comment embedded in a enum section")
+      print("Need to properly parse stuff, otherwise no problem")
+      stage = Stage.NOT_WORKING
+      finalStage = Stage.NOT_WORKING
+
+   # TODO: Maybe make a distinction between not working or disabled?
+   #       Do we run Versat for not working or is this return good?
+   if(finalStage == Stage.NOT_WORKING):
+      return OneTest(test,index,False,stage,True)
 
    versatResult = RunVersat(name,testTempDir,testInfoJson["default_args"])
-   if(versatResult.error):
+   if(versatResult.error or SIMULATE):
       return OneTest(test,index,False,stage,True)
 
    tokenAmount,hashVal = ComputeFilesTokenSizeAndHash(versatResult.filepaths)
@@ -258,15 +302,31 @@ if __name__ == "__main__":
    jsonfilePath = sys.argv[2]
 
    doUpdate = (command != "run-only") 
+   SIMULATE = (command == "simulate")
+
+   with open(jsonfilePath,"r") as file:
+      try:
+         testInfoJson = json.load(file)
+      except Exception as e:
+         print(f"Failed to parse testInfo file: {e}")
+         sys.exit(0)
+
+   allTestNames = [x['name'] for x in testInfoJson['tests']]
+
+   nameCount = {}
+   for name in allTestNames:
+      nameCount[name] = nameCount.get(name,0) + 1
+
+   repeatedElements = [x for x,y in nameCount.items() if y != 1]
+   if(len(repeatedElements)):
+      print("There are repeated tests:\n\t","\t\n".join(repeatedElements),sep='')
+      print("Exiting. Fix test info and run again.")
+      sys.exit(0)
+
+   # Put any check to the data above this line. # From this point assume data is correct
+   print("Gonna start running tests")
 
    if(command == "reset"):
-      with open(jsonfilePath,"r") as file:
-         try:
-            testInfoJson = json.load(file)
-         except Exception as e:
-            print(f"Failed to parse testInfo file: {e}")
-            sys.exit(0)
-
       for i in range(0,len(testInfoJson['tests'])):
          testInfoJson['tests'][i] = {
             'name' : testInfoJson['tests'][i]['name'],
@@ -278,67 +338,83 @@ if __name__ == "__main__":
 
       sys.exit(0)
 
-   with open(jsonfilePath,"r") as file:
-      try:
-         testInfoJson = json.load(file)
-      except Exception as e:
-         print(f"Failed to parse testInfo file: {e}")
-         sys.exit(0)
-
    # TODO: Pool waits until everything completes before producing output. Not a big fan.
    #       Rewrite to a work module that produces output as soon as possible.
    #       The writting of the json can be the last thing done, just the output to the terminal is the thing that needs to occur fast
    testResults = None
-   with multiprocessing.Pool(8) as p:
+
+   # Threads seem as fast as processes, maybe if we add more python logic this changes but for now keep threads. Processes was leaving some processes hanging, threads seem fine
+   #with multiprocessing.Pool(16) as p:
+   with ThreadPool(16) as p:
       testResults = p.map(ProcessOneTest,enumerate(testInfoJson['tests']))
 
-   def PadLeft(word,amount):
-      if(amount < len(word)):
-         return word[:amount]
-      res = '.' * (amount - len(word)) + word
-      return res
+   def GeneratePad(word,amount,padding = '.'):
+      return padding * (amount - len(word))
 
-   def PadRight(word,amount):
-      if(amount < len(word)):
-         return word[:amount]
-      res = word + '.' * (amount - len(word))
-      return res
-
-   firstColumnSize = 50
-   secondColumnSize = 10
-   thirdColumnSize = 60
-
+   testsOutput = []
    # We want output as fast as possible, so the report part of this code should move to the multiprocessing
-   for result in testResults:
+   for index,result in enumerate(testResults):
       test = result.test
       name = test['name']
-      finalStage = Stage[test.get('finalStage',"NOT_WORKING")]
+
+      finalStage = Stage.NOT_WORKING
+      try:
+         finalStage = Stage[test.get('finalStage',"NOT_WORKING")]
+      except Exception as e:
+         print("Error on Process One Test, probably a comment embedded in a enum section")
+         print("Need to properly parse stuff, otherwise no problem")
+         finalStage = Stage.NOT_WORKING
+
       stage = result.bestStageReached
 
-      testName = PadRight(name,firstColumnSize)
-      cached = PadRight("cached" if result.cached else "",secondColumnSize)
+      padding = ('.' if index % 2 == 0 else '-')
 
-      versatFail = PadLeft(f"{COLOR_RED}VERSAT_FAIL{COLOR_BASE}",thirdColumnSize)
-      failing = PadLeft(f"{COLOR_RED}FAIL{COLOR_BASE}",thirdColumnSize)
-      partial = PadLeft(f"{COLOR_YELLOW}PARTIAL{COLOR_BASE}({stage.name}/{finalStage.name})",thirdColumnSize)
-      ok = PadLeft(f"{COLOR_GREEN}OK{COLOR_BASE}",thirdColumnSize)
-      disabled = PadLeft(f"{COLOR_GREEN}DISABLED{COLOR_BASE}",thirdColumnSize)
+      testName = name
+      versatFail = "VERSAT_FAIL"
+      failing = "FAIL"
+      partial = "PARTIAL"
+      ok = "OK"
+      disabled = "DISABLED"
+      cached = "(cached)" if result.cached else ""
 
       condition = ok
 
+      color = COLOR_GREEN
       if(finalStage == Stage.NOT_WORKING):
          condition = disabled
+         color = COLOR_GREEN
       elif(stage == Stage.NOT_WORKING):
          condition = failing
+         color = COLOR_RED
       elif(stage != finalStage):
+         color = COLOR_YELLOW
          condition = partial
 
       if(result.error):
-         print(f"{testName}{cached}{versatFail}")
+         color = COLOR_RED
+         testsOutput.append((testName,versatFail,cached,color))
       else:
-         print(f"{testName}{cached}{condition}")
+         testsOutput.append((testName,condition,cached,color))
 
-      if(not result.error and not result.cached):
+   firstColumnSize = max([len(x[0]) for x in testsOutput]) + 1
+   secondColumnSize = max([len(x[1]) for x in testsOutput])
+
+   for output in testsOutput:
+      first = output[0]
+      second = output[1]
+      cached = output[2]
+      color = output[3]
+
+      firstPad = GeneratePad(first,firstColumnSize)
+      secondPad = GeneratePad(second,secondColumnSize) 
+
+      print(f"{first}{firstPad}{secondPad}{color}{second}{COLOR_BASE}{cached}")
+
+   if(SIMULATE):
+      sys.exit(0)
+
+   for result in testResults:
+      if(not result.error and not result.cached and not SIMULATE):
          testInfoJson['tests'][result.index]['tokens'] = result.tokens
          testInfoJson['tests'][result.index]['hash'] = result.hashVal
          testInfoJson['tests'][result.index]['stage'] = stage.name
